70c0d695b77e4c0f905e3d158f4466162ad31e30
Melih Emre Güler <melihguler@Melihs-MacBook-Pro.local>
2023-07-01 21:25:49 +0300

    test commit

--- app/src/main/java/com/example/warmthreminder/MainActivity.kt
+++ app/src/main/java/com/example/warmthreminder/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.warmthreminder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//test commit
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
